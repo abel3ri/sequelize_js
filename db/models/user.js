bea3a3f260498af0164f4d6b6b1ff9a07af6da3c
abel3ri <abelmerete@yahoo.com>
2024-08-10 01:34:54 +0300

    add config file, migration, model, and database file

---
+++ db/models/user.js
@@ -0,0 +1,46 @@
+"use strict";
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../database");
+
+module.exports = sequelize.define(
+  "user",
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    userType: {
+      type: DataTypes.ENUM("0", "1", "2"),
+    },
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    paranoid: true,
+    modelName: "user",
+    freezeTableName: true,
+  }
+);
