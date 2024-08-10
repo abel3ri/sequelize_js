c733c3b1d0638d38e2b79f76aa57ee1d9f6fef3a
abel3ri <abelmerete@yahoo.com>
2024-08-11 01:07:07 +0300

    use project router inside app

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRoutes");
+const projectRouter = require("./routes/projectRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
 
 app.use("*", (req, res, next) => {
   throw new AppError(`can't find ${req.originalUrl} on this server.`, 404);
