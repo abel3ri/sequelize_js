e67108b77dd45c416690273b6d39dba28662349b
abel3ri <abelmerete@yahoo.com>
2024-08-10 23:09:10 +0300

    use global error handler in the app:

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: "route not found!",
-  });
+  throw new AppError(`can't find ${req.originalUrl} on this server.`, 404);
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    status: "error",
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
