ee4cc5e5f8726ff81c0ed6b835ca029b24f312f7
abel3ri <abelmerete@yahoo.com>
2024-08-10 17:39:20 +0300

    add login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const authController = require("../controllers/authController");
 
 router.route("/signup").post(authController.signup);
+router.route("/login").post(authController.login);
 
 module.exports = router;
