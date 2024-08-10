807e866ece4f0cd4030c4eddb7d86c9c18301830
abel3ri <abelmerete@yahoo.com>
2024-08-10 23:07:55 +0300

    add validators and constraints

--- db/models/user.js
+++ db/models/user.js
@@ -2,11 +2,10 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../../database");
 const bcrypt = require("bcryptjs");
-const { promisify } = require("util");
+const AppError = require("../../utils/appError");
 
 module.exports = sequelize.define(
   "user",
-
   {
     id: {
       allowNull: false,
@@ -16,28 +15,78 @@ module.exports = sequelize.define(
     },
     userType: {
       type: DataTypes.ENUM("0", "1", "2"),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "userType can not be null",
+        },
+        notEmpty: {
+          msg: "userType can not be empty",
+        },
+      },
     },
     firstName: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "firstName can not be null",
+        },
+        notEmpty: {
+          msg: "firstName can not be empty",
+        },
+      },
     },
     lastName: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "lastName can not be null",
+        },
+        notEmpty: {
+          msg: "lastName can not be empty",
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
-      unique: true,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "email can not be null",
+        },
+        notEmpty: {
+          msg: "email can not be empty",
+        },
+        isEmail: {
+          msg: "please provide a valid email address",
+        },
+      },
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "password can not be null",
+        },
+        notEmpty: {
+          msg: "password can not be empty",
+        },
+      },
     },
     confirmPassword: {
       type: DataTypes.VIRTUAL,
       set(value) {
+        if (this.password.length < 8) {
+          throw new AppError("password must be at least 8 characters long", 400);
+        }
         if (value === this.password) {
           const hashedPassword = bcrypt.hashSync(value, 10);
           this.setDataValue("password", hashedPassword);
         } else {
-          throw new Error("password do not match.");
+          throw new AppError("password do not match", 400);
         }
       },
     },
