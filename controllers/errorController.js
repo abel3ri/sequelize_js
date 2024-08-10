d1580b7a46ad3dc261044ac56716de2a00c4a660
abel3ri <abelmerete@yahoo.com>
2024-08-10 23:02:35 +0300

    add global error handler

---
+++ controllers/errorController.js
@@ -0,0 +1,53 @@
+const sequelize = require("sequelize");
+const AppError = require("../utils/appError");
+
+const sendErrorDev = (err, res) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || "error";
+  const message = err.message;
+  const stack = err.stack;
+
+  return res.status(statusCode).json({
+    status,
+    message,
+    stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || "error";
+  const message = err.message;
+
+  if (err.isOperational) {
+    return res.status(statusCode).json({
+      status,
+      message,
+    });
+  }
+
+  console.log(err.name, err.message, err.stack);
+
+  return res.status(500).json({
+    status: "error",
+    message: "something went wrong",
+  });
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  if (err.name === "SequelizeUniqueConstraintError") {
+    const errorMsgs = err.errors.map((e) => e.message).join(", ");
+    err = new AppError(errorMsgs, 400);
+  } else if (err.name === "SequelizeValidationError") {
+    console.log(err.errors);
+    const errorMsgs = err.errors.map((e) => e.message).join(", ");
+    err = new AppError(errorMsgs, 400);
+  }
+  if (process.env.NODE_ENV == "development") {
+    return sendErrorDev(err, res);
+  }
+
+  return sendErrorProd(err, res);
+};
+
+module.exports = globalErrorHandler;
