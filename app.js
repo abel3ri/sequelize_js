f305e82c6aa641ba0bb86a000c62858827a1e68b
abel3ri <abelmerete@yahoo.com>
2024-08-10 17:41:11 +0300

    add global error handler

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRoutes");
 
+app.use(express.json({ limit: "10kb" }));
+
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res, next) => {
   res.status(404).json({
     status: "fail",
     message: "route not found!",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: "error",
+    message: err.message,
   });
 });
 
